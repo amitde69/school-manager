dbac6ea9b983675cf9b8abca66ddddcc1596f22c
amitde69 <amitde7896@gmail.com>
2022-06-18 23:29:04 +0300

    student add function

---
+++ classes/api/handlers/students.go
@@ -0,0 +1,60 @@
+
+package handlers
+import (
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"net/http"
+	"github.com/amitde69/school-manager/classes/api/resources"
+    "github.com/gin-gonic/gin"
+)
+
+func ListClasses(clientset client.Client, namespace string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+     	classes, err := resources.ListClasses(clientset, namespace)
+		if err != nil {
+			panic(err.Error())
+		}
+		c.IndentedJSON(http.StatusOK, classes)
+	}
+}
+
+func GetClass(clientset client.Client, namespace string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		className := c.Param("name")
+     	class, err := resources.GetClass(clientset, namespace, className)
+		if err != nil {
+			panic(err.Error())
+		}
+		if class.Name == "" {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Class not found"})
+		} else {
+			c.IndentedJSON(http.StatusOK, class)
+		}
+	}
+}
+
+func AddStudent(clientset client.Client, namespace string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		newStudent := resources.StudentAdd{}
+        c.BindJSON(&newStudent)
+		class, err := resources.AddStudent(clientset, namespace, newStudent.StudentName, newStudent.ClassName)
+		if err != nil {
+			panic(err.Error())
+		}
+		c.IndentedJSON(http.StatusOK, class)
+	}
+}
+
+func DeleteClass(clientset client.Client, namespace string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		className := c.Param("name")
+     	class, err := resources.DeleteClass(clientset, namespace, className)
+		if err != nil {
+			panic(err.Error())
+		}
+		if class.Name == "" {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Class not found"})
+		} else {
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "Class deleted"})
+		}
+	}
+}
---
+++ classes/api/resources/students.go
@@ -0,0 +1,123 @@
+package resources
+
+import (
+	"context"
+	"fmt"
+	// v1 "k8s.io/api/apps/v1"
+	// metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	// "k8s.io/client-go/kubernetes"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	classesapi "github.com/amitde69/school-manager/classes/controller/api/v1alpha1"
+	studentsapi "github.com/amitde69/school-manager/students/controller/api/v1alpha1"
+	// "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/labels"
+)
+
+// // list class resource using classesapi
+// func ListClasses(clientset client.Client, namespace string) ([]ClassRes, error) {
+// 	list := &classesapi.ClassList{}
+// 	err := clientset.List(context.Background(), list, client.InNamespace(namespace))
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	classesRes := make([]ClassRes, len(list.Items))
+// 	for i, class := range list.Items {
+// 		classesRes[i].Name = class.Spec.Name
+// 		classesRes[i].Teacher = class.Spec.Teacher
+// 		classesRes[i].Students = class.Spec.Students
+// 	}
+// 	// for _, class := range classes.Items {
+// 	// 	fmt.Printf("name: %s\nage: %s\n", class.Spec.Name, class.Spec.Age)
+// 	// }
+// 	return classesRes, err
+// }
+
+// // get class resource using classesapi
+// func GetClass(clientset client.Client, namespace string, className string) (ClassRes, error) {
+// 	// class := &classesapi.Class{}
+// 	classRes := ClassRes{}
+// 	classExists := &classesapi.ClassList{}
+// 	err := clientset.List(context.Background(), classExists, client.InNamespace(namespace), 
+// 		client.MatchingLabels(labels.Set{"app": "class-controller", "name": className}))
+// 	if err != nil {
+// 		return classRes, err
+// 	}
+// 	if len(classExists.Items) == 0 {
+// 		return classRes, err
+// 	}
+// 	classRes.Name = classExists.Items[0].Spec.Name
+// 	classRes.Teacher = classExists.Items[0].Spec.Teacher
+// 	classRes.Students = classExists.Items[0].Spec.Students
+// 	return classRes, err
+// }
+
+// // delete class resource using classesapi
+// func DeleteClass(clientset client.Client, namespace string, className string) (classesapi.Class, error) {
+// 	class := &classesapi.Class{}
+// 	classExists := &classesapi.ClassList{}
+// 	err := clientset.List(context.Background(), classExists, client.InNamespace(namespace), 
+// 		client.MatchingLabels(labels.Set{"app": "class-controller", "name": className}))
+// 	if err != nil {
+// 		return *class, err
+// 	}
+// 	if len(classExists.Items) == 0 {
+// 		return *class, err
+// 	}
+// 	class = &classExists.Items[0]
+// 	err = clientset.Delete(context.Background(), class)
+// 	return *class, err
+// }
+
+// create class resource using classesapi
+func AddStudent(clientset client.Client, namespace string, student string, class string) (ClassRes, error) {
+	classExists := &classesapi.ClassList{}
+	err := clientset.List(context.Background(), classExists, client.InNamespace(namespace), 
+		client.MatchingLabels(labels.Set{"app": "class-controller", "name": class}))
+	if len(classExists.Items) == 0 {
+		fmt.Printf("class not found")
+		return ClassRes{}, err
+	}
+	studentExists := &studentsapi.StudentList{}
+	err = clientset.List(context.Background(), studentExists, client.InNamespace(namespace), 
+		client.MatchingLabels(labels.Set{"app": "student-controller", "name": student}))
+	if len(studentExists.Items) == 0 {
+		fmt.Printf("student not found")
+		return ClassRes{}, err
+	}	
+	newClass := &classesapi.Class{}
+	newClass.Spec.Name = classExists.Items[0].Spec.Name
+	newClass.Spec.Teacher = classExists.Items[0].Spec.Teacher
+	newClass.Spec.Students = classExists.Items[0].Spec.Students
+	newClass.Spec.Students = append(newClass.Spec.Students, student)
+	newClass.ResourceVersion = classExists.Items[0].ResourceVersion
+	err = clientset.Update(context.Background(), newClass)
+	if err != nil {
+		return ClassRes{}, err
+	}
+	return ClassRes{}, err
+}
+
+
+// func newClassCR(namespace string, class *ClassRes) *classesapi.Class {
+// 	labels := map[string]string{
+// 		"app": "class-controller",
+// 		"name": class.Name,
+// 	}
+// 	return &classesapi.Class{
+// 		ObjectMeta: metav1.ObjectMeta{
+// 			Name: class.Name,
+// 			Namespace:    namespace,
+// 			Labels:       labels,
+// 		},
+// 		Spec: classesapi.ClassSpec{
+// 			Name: class.Name,
+// 			Teacher: class.Teacher,
+// 			Students: class.Students,
+// 		},
+// 	}
+// }
+
+type StudentAdd struct {
+	StudentName string `json:"studentname"`
+	ClassName string `json:"classname"`
+}
